79014acbf355ecb6b470d592bd97ad42fdacfac5
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-09-25 18:34:50 +0000

    completed the word search pair programming with new mocha tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+const transpose = function(matrix) {
+    let outputArray = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      outputArray.push([]);
+    }
+    for (let subArray of matrix) {
+      for (let k = 0; k < subArray.length; k++) {
+        outputArray[k].push(subArray[k]);
+      }
+    }
+    return outputArray;
+  };
+  
+  const wordSearch = (letters, word) => { 
+      if (!letters.length) {
+          return false;
+      }
+      const horizontalJoin = letters.map(ls => ls.join(''))
+      for (l of horizontalJoin) {
+          if (l.includes(word)) return true
+      }
+      const verticalJoin = transpose(letters).map(ls => ls.join(''));
+      for (l of verticalJoin) {
         if (l.includes(word)) return true
     }
-}
-
-module.exports = wordSearch
+      return false;
+  }
+  
+  module.exports = wordSearch;
+  
